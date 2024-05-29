dffa99e4c84becd2444acf2ce451e24b00ba9ee5
Q8er2y <155814328+Q8er2y@users.noreply.github.com>
2024-05-29 18:45:23 +0100

    making the mod buildable

--- src/mod/content/modBlocks.java
+++ src/mod/content/modBlocks.java
@@ -1,21 +1,52 @@
 package mod.content;
+import arc.graphics.Color;
+import mindustry.world.blocks.liquid.Conduit;
 import mod.content.modLiquids;
-import mod.content.modFx;
 import mindustry.content.Liquids;
+import mod.content.modFx;
+import arc.graphics.Color;
+import arc.graphics.gl.Shader;
+import mindustry.content.Fx;
+import mindustry.content.StatusEffects;
+import mindustry.entities.bullet.*;
+import mindustry.entities.effect.ParticleEffect;
+import mindustry.entities.effect.WaveEffect;
+import mindustry.entities.pattern.ShootBarrel;
+import mindustry.entities.pattern.ShootMulti;
+import mindustry.entities.pattern.ShootPattern;
 import mindustry.gen.Sounds;
+import mindustry.graphics.Pal;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
 import mindustry.type.LiquidStack;
 import mindustry.world.Block;
+import mindustry.world.blocks.defense.Wall;
+import mindustry.world.blocks.defense.turrets.ItemTurret;
+import mindustry.world.blocks.distribution.BufferedItemBridge;
+import mindustry.world.blocks.distribution.Conveyor;
+import mindustry.world.blocks.distribution.Router;
+import mindustry.world.blocks.environment.Floor;
+import mindustry.world.blocks.environment.OreBlock;
+import mindustry.world.blocks.environment.StaticWall;
+import mindustry.world.blocks.environment.SteamVent;
+import mindustry.world.blocks.heat.HeatProducer;
+import mindustry.world.blocks.liquid.Conduit;
+import mindustry.world.blocks.liquid.LiquidBridge;
+import mindustry.world.blocks.liquid.LiquidRouter;
 import mindustry.world.blocks.power.ConsumeGenerator;
-import mindustry.world.draw.DrawBlurSpin;
-import mindustry.world.draw.DrawDefault;
-import mindustry.world.draw.DrawMulti;
-import mindustry.world.meta.BlockGroup;
-
+import mindustry.world.blocks.production.*;
+import mindustry.world.blocks.storage.CoreBlock;
+import mindustry.world.blocks.units.UnitCargoLoader;
+import mindustry.world.blocks.units.UnitCargoUnloadPoint;
+import mindustry.world.blocks.units.UnitFactory;
+import mindustry.world.draw.*;
+import mindustry.world.meta.*;
 public class modBlocks {
     public static Block
             //power
-            steamTurbine;
-
+            steamTurbine,
+            //liquids
+            liquidPipe, liquidRouter, liquidBridge, liquidJunction;
     public static void load(){
         steamTurbine = new ConsumeGenerator("steam-turbine") {{
             group = BlockGroup.liquids;
@@ -24,19 +55,53 @@ public class modBlocks {
             effectChance = 0.6f;
             size = 3;
             ambientSound = Sounds.hum;
-            ambientSoundVolume = 0.1f;
+            ambientSoundVolume = 0.3f;
 
-            drawer = new DrawMulti(new DrawDefault(), new DrawBlurSpin("-rotator", 0.6f * 9f) {{
-                blurThresh = 0.01f;
+            drawer = new DrawMulti(new DrawDefault(), new DrawBlurSpin("-rotator", 9f) {{
+                blurThresh = 0.10f;
 
 
             hasLiquids = true;
             outputLiquid = new LiquidStack(Liquids.water, 0.25f);
             consumeLiquid(modLiquids.steam, 1f);
             liquidCapacity = 20f;
-            fogRadius = 3;
+            fogRadius = 2;
             alwaysUnlocked = true;
         }});
+                liquidPipe = new Conduit("liquid-pipe"){{
+                    health = 100;
+                    botColor = Color.valueOf("262525");
+                    bridgeReplacement = liquidBridge;
+                    junctionReplacement = liquidJunction;
+                    liquidCapacity = 10f;
+                    liquidPressure = 10f;
+                    size = 1;
+                }};
+                liquidRouter = new LiquidRouter("liquid-router"){{
+                    health = 143;
+                    size = 1;
+                }};
+
+                liquidBridge = new LiquidBridge("liquid-bridge"){{
+                    fadeIn = moveArrows = false;
+                    arrowSpacing = 6f;
+                    range = 5;
+                    hasPower = false;
+                }};
+                //endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
         }};
     }
 }
